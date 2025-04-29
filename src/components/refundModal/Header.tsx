359b10d8dac4185d228642b98d1f1b3f426ad2ca
LeeJinyoung218 <amy021899@gmail.com>
2025-04-29 17:04:20 +0900

    design: 코인 카드 디자인 수정

--- src/components/refundModal/Header.tsx
+++ src/components/refundModal/Header.tsx
@@ -7,14 +7,14 @@ export default function Header({ moveToAddAccount, moveToRefund }: HeaderProps) 
   return (
     <>
       {/* Balance Card */}
-      <div className="flex items-stretch mb-6 bg-white rounded-2xl shadow-sm overflow-hidden min-w-fit">
+      <div className="flex items-stretch mb-6 bg-white rounded-[20px] shadow-sm overflow-hidden min-w-fit border-pink-400 border-2">
         <div className="flex-1 p-5">
           <p className="text-sm font-medium text-gray-700">보유 코인</p>
           <h1 className="text-4xl font-bold mt-1 whitespace-nowrap">
             10000<span className="text-2xl">코인</span>
           </h1>
         </div>
-        <div className="bg-pink-400 rounded-r-[20px] flex w-fit">
+        <div className="bg-pink-400 flex w-fit">
           <button
             type="button"
             className="text-white font-medium text-xl px-6 h-full flex items-center justify-center min-w-fit whitespace-nowrap"
