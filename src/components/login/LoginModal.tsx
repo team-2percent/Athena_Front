8792d0c91bf3e6debf74626aa6977193b2f67136
LeeJinyoung218 <amy021899@gmail.com>
2025-05-04 01:43:09 +0900

    fix: 버튼 타입 속성 추가 및 구분선 요소 셀프 닫기로 수정

--- src/components/login/LoginModal.tsx
+++ src/components/login/LoginModal.tsx
@@ -37,7 +37,7 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
           }}
         >
           {/* Close button */}
-          <button onClick={onClose} className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
+          <button type="button" onClick={onClose} className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
             <X size={24} />
           </button>
 
@@ -74,9 +74,9 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
 
           {/* Divider */}
           <div className="flex items-center mb-8">
-            <div className="flex-1 h-px bg-gray-300"></div>
+            <div className="flex-1 h-px bg-gray-300" />
             <span className="px-4 text-gray-400">아니면</span>
-            <div className="flex-1 h-px bg-gray-300"></div>
+            <div className="flex-1 h-px bg-gray-300" />
           </div>
 
           {/* Kakao Login Button */}
