bbd21875db6dca1fbb18d7af86554cd4868cebbb
LeeJinyoung218 <amy021899@gmail.com>
2025-05-07 12:57:27 +0900

    feat: 정렬 기능 구현 및 공통 컴포넌트 ListPage 구현, 목록 페이지 통일

---
+++ src/app/deadline/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import ListPage from "@/components/list/ListPage";
+
+export default function DeadlinePage() {
+    return (
+        <ListPage type="deadline" />
+    )
+}
--- src/components/list/ProductsList.tsx
+++ src/components/list/ProductsList.tsx
@@ -1,3 +1,4 @@
+import EmptyMessage from "../common/EmptyMessage"
 import ListProduct from "./ListProduct"
 
 export default function ProductsList({ products }: {
@@ -14,20 +15,24 @@ export default function ProductsList({ products }: {
 }) {
   return (
     <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
-      <div className="w-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-20 gap-x-[10px] justify-items-center">
-        {products.map((product) => (
-          <ListProduct 
-            key={product.id}
-            id={product.id}
-            image={product.image}
-            sellerName={product.sellerName}
-            productName={product.productName}
-            achievementRate={product.achievementRate}
-            description={product.description}
-            liked={false}
-            daysLeft={product.daysLeft} />
-        ))}
-      </div>
+        {
+            products.length === 0 ?
+            <EmptyMessage message="상품이 없습니다." /> :
+            <div className="w-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-20 gap-x-[10px] justify-items-center">
+                {products.map((product) => (
+                <ListProduct 
+                    key={product.id}
+                    id={product.id}
+                    image={product.image}
+                    sellerName={product.sellerName}
+                    productName={product.productName}
+                    achievementRate={product.achievementRate}
+                    description={product.description}
+                    liked={false}
+                    daysLeft={product.daysLeft} />
+                ))}
+            </div>
+        }
     </div>
   )
 }
