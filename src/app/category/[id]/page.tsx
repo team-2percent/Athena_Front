bd72f4c31b7ebc2596cce76693cc451e0e80b242
LeeJinyoung218 <amy021899@gmail.com>
2025-05-07 13:39:14 +0900

    feat: 카테고리 메뉴바 구현 및 카테고리 페이지 구현

---
+++ src/app/category/[id]/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import CategoryMenu from "@/components/list/CategoryMenu";
+import ListPage from "@/components/list/ListPage";
+import { useParams } from "next/navigation";
+
+export default function CategoryPage() {
+    const params = useParams<{ id: string }>()
+    const categoryId = parseInt(params.id)
+    return (
+        <div className="flex flex-col items-center">
+            {/* 카테고리 메뉴 */}
+            <CategoryMenu categoryId={categoryId} />
+            <ListPage type="category" categoryId={categoryId} />
+        </div>
+    )
+}
