ca033c98e8d9105e67ac3604778ca40f62341b3f
LeeJinyoung218 <amy021899@gmail.com>
2025-05-09 10:24:17 +0900

    design: 편집 페이지 배송지 메뉴 레이아웃 변경, 배송지 추가 버튼 변경

--- src/components/profileEdit/AddressInfo.tsx
+++ src/components/profileEdit/AddressInfo.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Plus, Trash2 } from "lucide-react"
+import { Plus, Search, Trash2 } from "lucide-react"
 import { useState } from "react"
 import AddressModal from "./AddressModal"
 
@@ -36,11 +36,20 @@ export default function AddressInfo() {
         },
     ])
 
+    const [newAddress, setNewAddress] = useState<Omit<AddressInfo, "id" | "isDefault">>({
+        name: "",
+        address: "",
+        detailAddress: "",
+        zipCode: "",
+    })
+
+    const isSaveDisabled = newAddress.name.length === 0 || newAddress.address.length === 0 || newAddress.detailAddress.length === 0;
+
     // 배송지 추가 모달 열기 핸들러
     const handleOpenAddressModal = () => {
         setIsAddressModalOpen(true)
     }
-    
+
     // 배송지 삭제 핸들러
     const handleRemoveAddress = (id: string) => {
         const updatedAddresses = addresses.filter((address) => address.id !== id)
@@ -63,90 +72,141 @@ export default function AddressInfo() {
         )
     }
 
-    // 검색된 주소 추가 핸들러
-    const handleAddSearchedAddress = (formData: AddressAddInfo) => {
-        const newAddress: AddressInfo = {
-        id: Date.now().toString(),
-        name: formData.name,
-        address: formData.address,
-        detailAddress: formData.detailAddress,
-        zipCode: formData.zipCode,
-        isDefault: addresses.length === 0,
-        }
-
-        setAddresses([...addresses, newAddress])
+    // 주소 추가 핸들러
+    const handleAddAddress = () => {
+        setAddresses([...addresses, 
+            {
+                id: Date.now().toString(),
+                name: newAddress.name,
+                address: newAddress.address,
+                detailAddress: newAddress.detailAddress,
+                zipCode: newAddress.zipCode,
+                isDefault: addresses.length === 0,
+                }
+        ])
         setIsAddressModalOpen(false)
     }
 
+    const handleComplete = (data: any) => {
+        setNewAddress({
+            ...newAddress,
+            address: data.address,
+            zipCode: data.zonecode,
+        })
+    }
+
     return (
-        <div className="bg-white rounded-lg shadow p-6 space-y-4">
-            <div className="flex justify-between items-center border-b pb-2">
-                <h2 className="text-lg font-medium">배송지 정보</h2>
-            </div>
-
-            {/* 배송지 추가 버튼 */}
-            <div className="flex justify-center">
-                <button
-                type="button"
-                onClick={handleOpenAddressModal}
-                className="px-4 py-2 bg-pink-500 text-white rounded-md hover:bg-pink-600 text-sm flex items-center"
-                >
-                <Plus className="w-4 h-4 mr-1" /> 배송지 추가
-                </button>
-            </div>
-
-            {/* 배송지 목록 */}
-            <h3 className="text-sm font-medium mt-4">등록된 배송지 목록</h3>
-            {addresses.length === 0 ? 
-                <p className="text-gray-500 text-center py-4">등록된 배송지가 없습니다.</p>
-             : 
-                <div className="space-y-4 mt-2">
-                    {addresses.map(address => (
-                        <div key={address.id} className="border rounded-md p-4 relative">
-                            <div className="flex justify-between items-start">
-                                <div className="flex items-start">
-                                    <input
-                                        type="radio"
-                                        id={`default-address-${address.id}`}
-                                        name="default-address"
-                                        checked={address.isDefault}
-                                        onChange={() => handleSetDefaultAddress(address.id)}
-                                        className="w-4 h-4 text-pink-500 border-gray-300 focus:ring-pink-300 mt-1 mr-3"
-                                    />
-                                    <div>
-                                        <div className="flex items-center">
-                                            <p className="font-medium">{address.name}</p>
-                                        {address.isDefault && 
-                                            <span className="ml-2 px-2 py-0.5 bg-pink-100 text-pink-600 text-xs rounded-full">기본</span>
-                                        }
-                                        </div>
-                                        <p className="text-sm text-gray-500 mt-1">
-                                        [{address.zipCode}] {address.address}
-                                        </p>
-                                        {address.detailAddress && <p className="text-sm text-gray-500">{address.detailAddress}</p>}
-                                    </div>
-                                </div>
-                                <button
+        <div className="flex gap-4">
+            <div className="flex-1 bg-white rounded-lg shadow py-6 px-10">
+                <h3 className="text-lg font-medium mb-6">배송지 추가</h3>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            배송지명
+                        </label>
+                        <input
+                            type="text"
+                            value={newAddress.name}
+                            onChange={(e) => setNewAddress({
+                                ...newAddress,
+                                name: e.target.value,
+                            })}
+                            className="w-full flex-1 px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-300"
+                        />
+                    </div>
+                    
+                    <div className="flex w-full my-3 gap-2 justify-between items-center my-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            주소
+                        </label>
+                        <p className="block text-sm font-medium text-gray-700">{ newAddress.address.length > 0 && `[${newAddress.zipCode}] ${newAddress.address}`}</p>
+                        <button
+                            type="button"
+                            onClick={handleOpenAddressModal}
+                            className="bg-pink-500 text-white px-2 py-2 rounded-md hover:bg-pink-600"
+                        >
+                            <Search className="w-4 h-4" />
+                        </button>
+                    </div>
+                    
+                    <div className="space-y-4">
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                            상세주소
+                        </label>
+                        <input
+                            type="text"
+                            value={newAddress.detailAddress}
+                            onChange={(e) => setNewAddress({
+                                ...newAddress,
+                                detailAddress: e.target.value,
+                            })}
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-300"
+                        />
+                        <div className="mt-4 flex justify-end">
+                            <button
                                 type="button"
-                                    onClick={() => handleRemoveAddress(address.id)}
-                                    className="text-gray-400 hover:text-red-500"
-                                >
-                                    <Trash2 className="w-4 h-4" />
-                                </button>
-                            </div>
+                                disabled={isSaveDisabled}
+                                onClick={handleAddAddress}
+                                className="px-4 py-2 bg-pink-500 text-white rounded-md hover:bg-pink-600 text-sm flex items-center"
+                            >
+                                <Plus className="w-4 h-4 mr-1" /> 배송지 추가
+                            </button>
                         </div>
-                    ))}
-                </div>
-            }
+                    </div>
+            </div>
 
-      
+            <div className="flex-1 bg-white rounded-lg shadow py-6 px-10">
+                
 
+                {/* 배송지 목록 */}
+                <h3 className="text-lg font-medium mb-6">등록된 배송지 목록</h3>
+                {addresses.length === 0 ? 
+                    <p className="text-gray-500 text-center py-4">등록된 배송지가 없습니다.</p>
+                : 
+                    <div className="space-y-4 mt-2">
+                        {addresses.map(address => (
+                            <div key={address.id} className="border rounded-md p-4 relative">
+                                <div className="flex justify-between items-start">
+                                    <div className="flex items-start">
+                                        <input
+                                            type="radio"
+                                            id={`default-address-${address.id}`}
+                                            name="default-address"
+                                            checked={address.isDefault}
+                                            onChange={() => handleSetDefaultAddress(address.id)}
+                                            className="w-4 h-4 text-pink-500 border-gray-300 focus:ring-pink-300 mt-1 mr-3"
+                                        />
+                                        <div>
+                                            <div className="flex items-center">
+                                                <p className="font-medium">{address.name}</p>
+                                            {address.isDefault && 
+                                                <span className="ml-2 px-2 py-0.5 bg-pink-100 text-pink-600 text-xs rounded-full">기본</span>
+                                            }
+                                            </div>
+                                            <p className="text-sm text-gray-500 mt-1">
+                                            [{address.zipCode}] {address.address}
+                                            </p>
+                                            {address.detailAddress && <p className="text-sm text-gray-500">{address.detailAddress}</p>}
+                                        </div>
+                                    </div>
+                                    <button
+                                    type="button"
+                                        onClick={() => handleRemoveAddress(address.id)}
+                                        className="text-gray-400 hover:text-red-500"
+                                    >
+                                        <Trash2 className="w-4 h-4" />
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                }
+            </div>
             {/* 배송지 주소 모달 */}
             {isAddressModalOpen && 
                 <AddressModal
                 isOpen={isAddressModalOpen}
                 onClose={() => setIsAddressModalOpen(false)}
-                onAdd={handleAddSearchedAddress}
+                onComplete={handleComplete}
             />}
         </div>
     )
