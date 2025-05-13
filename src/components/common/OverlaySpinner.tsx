8ea6af6d3cddc6b2a4321d790794555afe5a823c
LeeJinyoung218 <amy021899@gmail.com>
2025-05-14 00:41:35 +0900

    fix: OverlaySpinner 렌더링 시 외부 스크롤 방지

--- src/components/common/OverlaySpinner.tsx
+++ src/components/common/OverlaySpinner.tsx
@@ -1,12 +1,25 @@
 "use client"
 
+import { useEffect } from "react"
 import Spinner from "./Spinner"
 
 interface Props {
     message: string
 }
 
+
+
 export default function OverlaySpinner({ message }: Props) {
+    // 모달 뒷배경 스크롤 방지
+    useEffect(() => {
+        document.body.style.overflow = "hidden"
+
+        return () => {
+        // 모달이 닫힐 때 body 스크롤 복원
+        document.body.style.overflow = ""
+        }
+    }, [])
+
     return (
         <div className="fixed inset-0 bg-white/20 flex items-center justify-center z-50">
             <Spinner message={message} />
