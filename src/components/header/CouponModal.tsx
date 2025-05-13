79d458ef3c2f0b50ed846f2b4d1052d71f828fc8
LeeJinyoung218 <amy021899@gmail.com>
2025-05-13 15:32:27 +0900

    feat: 쿠폰 없음 상태 추가, loading spinner 추가

--- src/components/header/CouponModal.tsx
+++ src/components/header/CouponModal.tsx
@@ -4,6 +4,8 @@ import clsx from "clsx"
 import { Percent } from "lucide-react"
 import { useEffect, useState } from "react"
 import { CouponEvent } from "@/lib/couponInterface"
+import EmptyMessage from "../common/EmptyMessage"
+import Spinner from "../common/Spinner"
 
 interface CouponProps {
   isOpen: boolean
@@ -12,12 +14,53 @@ interface CouponProps {
 
 export default function CouponModal({ isOpen, onClose }: CouponProps) {
   if (!isOpen) return null
-  const { apiCall } = useApi();
+  const { apiCall, isLoading } = useApi();
   const [coupons, setCoupons] = useState<CouponEvent[]>([]);
 
   const handleGetCoupon = (couponId: number) => {
     // 쿠폰 발급 로직 추후 추가
     console.log(`쿠폰 ${couponId} 발급됨`)
+  }
+
+  const renderCoupons = () => {
+    if (isLoading) {
+      return <Spinner message="쿠폰 목록을 불러오고 있습니다."/>
+    } else if (coupons.length === 0) {
+      return <EmptyMessage message="발급 가능한 쿠폰이 없습니다." />
+    } else {
+      return coupons.map((coupon) => (
+        <div key={coupon.id} className="rounded-xl border p-3">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col">
+            <div className="flex items-center gap-3">
+              <div className={clsx("flex h-10 w-10 items-center justify-center rounded-md text-white", coupon.userIssued ? "bg-disabled-background" : "bg-main-color")}>
+                <Percent className="h-5 w-5" />
+              </div>
+              <div>
+                <div className={clsx("text-lg font-bold", coupon.userIssued ? "text-disabled-color" : "text-main-color")}>{coupon.price} 원</div>
+                <div className="text-sm font-medium">{coupon.title}</div>
+              </div>
+              </div>
+              <p className="mt-1 text-xs text-sub-gray">{coupon.content}</p>
+              <p className="mt-0.5 text-xs text-sub-gray">{new Date(coupon.expireAt).toLocaleString('ko-KR', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).replace(/\//g, '.').replace(',', '') + ' 만료'}</p>
+            </div>
+            <div className="flex flex-col items-end">
+              <button
+                  type="button"
+                  className={clsx("rounded-full w-fit px-4 py-1.5 text-sm font-medium", coupon.userIssued ? "bg-disabled-background text-disabled-color" : "bg-main-color text-white")}
+                  onClick={() => handleGetCoupon(coupon.id)}
+              >
+                {coupon.userIssued ? "발급완료" : "발급받기"}
+              </button>
+              <div className="mt-1 text-xs text-sub-gray">
+                {coupon.stock}개 남음
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      ))
+    }
   }
   useEffect(() => {
     apiCall("/api/couponEvent/getActives", "GET").then(({ data }) => {
@@ -37,38 +80,7 @@ export default function CouponModal({ isOpen, onClose }: CouponProps) {
         <h2 className="mb-4 text-left text-2xl font-bold">할인 쿠폰</h2>
 
         <div className="space-y-3">
-          {coupons.map((coupon) => (
-            <div key={coupon.id} className="rounded-xl border p-3">
-              <div className="flex items-center justify-between">
-                <div className="flex flex-col">
-                <div className="flex items-center gap-3">
-                  <div className={clsx("flex h-10 w-10 items-center justify-center rounded-md text-white", coupon.userIssued ? "bg-disabled-background" : "bg-main-color")}>
-                    <Percent className="h-5 w-5" />
-                  </div>
-                  <div>
-                    <div className={clsx("text-lg font-bold", coupon.userIssued ? "text-disabled-color" : "text-main-color")}>{coupon.price} 원</div>
-                    <div className="text-sm font-medium">{coupon.title}</div>
-                  </div>
-                  </div>
-                  <p className="mt-1 text-xs text-sub-gray">{coupon.content}</p>
-                  <p className="mt-0.5 text-xs text-sub-gray">{new Date(coupon.expireAt).toLocaleString('ko-KR', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).replace(/\//g, '.').replace(',', '') + ' 만료'}</p>
-                </div>
-                <div className="flex flex-col items-end">
-                  <button
-                      type="button"
-                      className={clsx("rounded-full w-fit px-4 py-1.5 text-sm font-medium", coupon.userIssued ? "bg-disabled-background text-disabled-color" : "bg-main-color text-white")}
-                      onClick={() => handleGetCoupon(coupon.id)}
-                  >
-                    {coupon.userIssued ? "발급완료" : "발급받기"}
-                  </button>
-                  <div className="mt-1 text-xs text-sub-gray">
-                    {coupon.stock}개 남음
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-          ))}
+          {renderCoupons()}
         </div>
 
         <div className="mt-4 flex justify-end">
