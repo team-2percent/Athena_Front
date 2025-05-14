f900500566d3150f9765c441fc4393ae3d6012bf
LeeJinyoung218 <amy021899@gmail.com>
2025-05-14 12:59:53 +0900

    feat: 프로젝트 리스트의 로딩 상태 추가

--- src/components/list/ProjectsList.tsx
+++ src/components/list/ProjectsList.tsx
@@ -1,4 +1,5 @@
 import EmptyMessage from "../common/EmptyMessage"
+import Spinner from "../common/Spinner"
 import ListProject from "./ListProjectCard"
 
 export default function ProjectsList({ projects, isLoading }: {
@@ -14,11 +15,11 @@ export default function ProjectsList({ projects, isLoading }: {
     }[]
     isLoading: boolean
 }) {
-  return (
-    <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
-        {
-            projects.length === 0 && !isLoading ?
-            <EmptyMessage message="상품이 없습니다." /> :
+    const renderList = () => {
+        if (isLoading) return <Spinner message="프로젝트를 불러오는 중입니다." />
+        else if (projects.length === 0) return <EmptyMessage message="프로젝트가 없습니다." />
+        else 
+        return (
             <div className="w-fit grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-y-20 gap-x-[10px] justify-items-center">
                 {projects.map((project) => (
                 <ListProject 
@@ -33,6 +34,12 @@ export default function ProjectsList({ projects, isLoading }: {
                     daysLeft={project.daysLeft} />
                 ))}
             </div>
+        )
+    }
+  return (
+    <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
+        {
+            renderList()
         }
     </div>
   )
