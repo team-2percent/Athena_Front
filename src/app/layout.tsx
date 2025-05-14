e93ce17223c85a3ffaf75d73b271f251746d67a1
7minseok7 <mansang99@gmail.com>
2025-05-14 15:22:29 +0900

    feat: 카카오페이 결제 api 로직 및 mock api 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import HeaderLoader from "@/components/HeaderLoader";
 import RegisterPageButtonLoader from "@/components/RegisterPageButtonLoader";
 
+// 로그인 토큰 관련
+import { AuthProvider } from '../contexts/AuthContext';
+
 // Pretendard 폰트를 사용하기 위해 불러옴
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
@@ -27,13 +30,15 @@ export default function RootLayout({
       <body
         className={`${pretendard.variable} font-pretendard`} // Pretendard 사용
       >
-        <HeaderLoader />
-        <main className="flex-1 bg-white mt-1">
-          <div className="container mx-auto px-4 py-8">
-            {children}
-          </div>
-          <RegisterPageButtonLoader />
-        </main>
+        <AuthProvider>
+          <HeaderLoader />
+          <main className="flex-1 bg-white mt-1">
+            <div className="container mx-auto px-4 py-8">
+              {children}
+            </div>
+            <RegisterPageButtonLoader />
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
