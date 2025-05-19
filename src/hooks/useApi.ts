b4d9b44317a451063ad8b7243b28dfa536d00fa0
LeeJinyoung218 <amy021899@gmail.com>
2025-05-19 17:46:22 +0900

    fix: axios 적용

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
-// import { useAuth } from "../contexts/AuthContext";
 import useAuthStore from "@/stores/auth";
+import axios from '@/lib/customAxios';
 
 interface ApiResponse<T> {
   data: T | null;
@@ -9,34 +9,53 @@ interface ApiResponse<T> {
   status: number;
 }
 
-const api_base = "https://athena.i-am-jay.com";
+type MethodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
+
+const getResponse = {
+  "GET": async (url: string) => {
+    return await axios.get(url);
+  },
+  "POST": async (url: string, body: object | null) => {
+    return await axios.post(url, body);
+  },
+  "PUT": async (url: string, body: object | null) => {
+    return await axios.put(url, body);
+  },
+  "DELETE": async (url: string) => {
+    return await axios.delete(url);
+  },
+  "PATCH": async (url: string, body: object | null) => {
+    return await axios.patch(url, body)
+  }
+}
 
 export function useApi() {
   const { logout } = useAuthStore();
   const [isLoading, setIsLoading] = useState(false);
 
-  const refreshAccessToken = async () => {
-    const refreshToken = localStorage.getItem("refreshToken");
-    if (!refreshToken) {
-      throw new Error("No refresh token available");
-    }
+  // refreshToken 관련 로직. 구현, 수정 후 주석 풀어주세요. 
+  // const refreshAccessToken = async () => {
+  //   const refreshToken = localStorage.getItem("refreshToken");
+  //   if (!refreshToken) {
+  //     throw new Error("No refresh token available");
+  //   }
 
-    const response = await fetch(api_base + "/api/accounts/token/refresh/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ refresh: refreshToken }),
-    });
+  //   const response = await fetch(api_base + "/api/accounts/token/refresh/", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ refresh: refreshToken }),
+  //   });
 
-    if (!response.ok) {
-      throw new Error("Failed to refresh token");
-    }
+  //   if (!response.ok) {
+  //     throw new Error("Failed to refresh token");
+  //   }
 
-    const data = await response.json();
-    localStorage.setItem("accessToken", data.access);
-    return data.access;
-  };
+  //   const data = await response.json();
+  //   localStorage.setItem("accessToken", data.access);
+  //   return data.access;
+  // };
 
   const apiCall = useCallback(
     async <T>(
@@ -50,39 +69,43 @@ export function useApi() {
         const makeRequest = async (
           token: string | null
         ): Promise<ApiResponse<T>> => {
-          const response = await fetch(api_base + url, {
-            method, // 여기서 전달받은 method를 사용합니다.
-            headers: {
-              "Content-Type": "application/json",
-              ...(token ? { Authorization: `Bearer ${token}` } : {}),
-            },
-            ...(body ? { body: JSON.stringify(body) } : {}),
-          });
+          // const response = await fetch(api_base + url, {
+          //   method, // 여기서 전달받은 method를 사용합니다.
+          //   headers: {
+          //     "Content-Type": "application/json",
+          //     ...(token ? { Authorization: `Bearer ${token}` } : {}),
+          //   },
+          //   ...(body ? { body: JSON.stringify(body) } : {}),
+          // });
 
-          if (response.status === 204) {
-            return { data: null, error: null, isLoading: false, status: 204 };
-          }
+          try {
+            const response = await getResponse[method as MethodType](url, body);
 
-          const responseData = await response.json();
-
-          if (!response.ok) {
-            if (response.status === 401) {
-              throw new Error("Unauthorized");
+            if (response.status === 204) {
+              return { data: null, error: null, isLoading: false, status: 204 };
             }
+
             return {
-              data: responseData,
-              error: responseData.message || "API request failed",
+              data: response.data,
+              error: null,
               isLoading: false,
               status: response.status,
             };
+          } catch (err: any) {
+            if (err.response) {
+              if (err.response.status === 401) {
+                throw new Error("Unauthorized");
+              }
+  
+              return {
+                data: null,
+                error: err.response.data.message || "API request failed",
+                isLoading: false,
+                status: err.response.status,
+              };
+            }
+            throw err;
           }
-
-          return {
-            data: responseData,
-            error: null,
-            isLoading: false,
-            status: response.status,
-          };
         };
 
         try {
@@ -90,30 +113,31 @@ export function useApi() {
           setIsLoading(false);
           return result;
         } catch (error) {
-          if (error instanceof Error && error.message === "Unauthorized") {
-            // Token might be expired, try to refresh
-            try {
-              accessToken = await refreshAccessToken();
-              const result = await makeRequest(accessToken);
-              setIsLoading(false);
-              return result;
-            } catch (refreshError) {
-              if (
-                refreshError instanceof Error &&
-                refreshError.message === "Failed to refresh token"
-              ) {
-                logout();
-                setIsLoading(false);
-                return {
-                  data: null,
-                  error: "Authentication failed. Please log in again.",
-                  isLoading: false,
-                  status: 401,
-                };
-              }
-              throw refreshError;
-            }
-          }
+          // refresh 관련 로직, 추후 수정 및 주석 해제 해주세요.
+          // if (error instanceof Error && error.message === "Unauthorized") {
+          //   // Token might be expired, try to refresh
+          //   try {
+          //     accessToken = await refreshAccessToken();
+          //     const result = await makeRequest(accessToken);
+          //     setIsLoading(false);
+          //     return result;
+          //   } catch (refreshError) {
+          //     if (
+          //       refreshError instanceof Error &&
+          //       refreshError.message === "Failed to refresh token"
+          //     ) {
+          //       logout();
+          //       setIsLoading(false);
+          //       return {
+          //         data: null,
+          //         error: "Authentication failed. Please log in again.",
+          //         isLoading: false,
+          //         status: 401,
+          //       };
+          //     }
+          //     throw refreshError;
+          //   }
+          // }
           throw error;
         }
       } catch (error) {
