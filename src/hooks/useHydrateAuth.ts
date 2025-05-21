978caefa16a09b18b492f67f916cb2bd6fc47925
LeeJinyoung218 <amy021899@gmail.com>
2025-05-22 02:04:41 +0900

    fix: login 시 localStorage에 userId 추가

--- src/hooks/useHydrateAuth.ts
+++ src/hooks/useHydrateAuth.ts
@@ -3,18 +3,20 @@ import { jwtDecode } from "jwt-decode";
 import useAuthStore from "@/stores/auth";
 
 export function useHydrateAuth() {
-  const setLoggedIn = useAuthStore((s) => s.setLoggedIn);
-  const setRole = useAuthStore((s) => s.setRole);
+  const {setLoggedIn, setRole, setUserId} = useAuthStore();
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
+    const userId = localStorage.getItem("userId");
 
-    if (accessToken) {
+    if (accessToken && userId) {
       setLoggedIn(true);
       try {
         const { role } = jwtDecode<{ role: "ROLE_ADMIN" | "ROLE_USER" }>(accessToken);
         setRole(role);
+        setUserId(+userId);
       } catch {
         setRole("");
+        setUserId(null);
       }
     } else {
       setLoggedIn(false);
