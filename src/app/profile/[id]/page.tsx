c7b6d58c5820a8d6eebe46d413a87f3c83dd88fc
LeeJinyoung218 <amy021899@gmail.com>
2025-05-23 12:35:49 +0900

    fix: 프로필 편집 메뉴도 재사용 컴포넌트로 전환, 안쓰는 컴포넌트 삭제

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -59,7 +59,7 @@ export default function ProfilePage() {
   }
 
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white w-[var(--content-width)]">
       <div className="container my-8 mx-auto px-4">
         {/* 프로필 상단 영역 */}
         <ProfileHeader
