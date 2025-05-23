d3809a388175dc4f279cf37a23dc4cf1c2a395cb
LeeJinyoung218 <amy021899@gmail.com>
2025-05-23 13:40:40 +0900

    design: 누락 수정

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -2,6 +2,8 @@ import ListPage from "@/components/list/ListPage";
 
 export default function NewPage() {
     return (
+        <div className="flex flex-col items-center w-[var(--content-width)]">
         <ListPage type="new" />
+        </div>
     )
 }
