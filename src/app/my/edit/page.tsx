0218f679deda3357d73c8dc9de4168a3b6822715
LeeJinyoung218 <amy021899@gmail.com>
2025-05-29 01:03:55 +0900

    feat: 프로필 편집 페이지 내 돌아가기 버튼 추가

--- src/app/my/edit/page.tsx
+++ src/app/my/edit/page.tsx
@@ -5,9 +5,12 @@ import ProfileInfo from "@/components/profileEdit/ProfileInfo"
 import AccountInfo from "@/components/profileEdit/AccountInfo"
 import AddressInfo from "@/components/profileEdit/AddressInfo"
 import MenuTab from "@/components/common/MenuTab"
+import { ArrowLeftIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function EditPage() {
     const [activeTab, setActiveTab] = useState("프로필")
+    const router = useRouter();
 
     const onMenuChange = (tab: string) => {
         setActiveTab(tab)
@@ -26,6 +29,12 @@ export default function EditPage() {
         }
     }
     return <div className="h-full mt-8 w-[var(--content-width)] mx-auto">
+        <div className="flex w-full mb-5">
+            <button className="text-sm text-gray-500 flex items-center gap-2" onClick={() => router.push("/my")}>
+                <ArrowLeftIcon className="w-4 h-4" />
+                프로필로 돌아가기
+            </button>
+        </div>
         <MenuTab tabs={["프로필", "계좌", "배송지"]} activeTab={activeTab} onClickTab={onMenuChange} className="border-b border-gray-border"/>
         <div className="w-full ax-w-6xl pt-10 h-full">
             {renderContent()}
