61da65899845199bcefd806c564a5029bacb9005
7minseok7 <mansang99@gmail.com>
2025-05-29 22:44:39 +0900

    feat: 회원 탈퇴 화면만 구현

--- src/app/my/edit/page.tsx
+++ src/app/my/edit/page.tsx
@@ -5,6 +5,7 @@ import ProfileInfo from "@/components/profileEdit/ProfileInfo"
 import AccountInfo from "@/components/profileEdit/AccountInfo"
 import AddressInfo from "@/components/profileEdit/AddressInfo"
 import MenuTab from "@/components/common/MenuTab"
+import WithdrawInfo from "@/components/profileEdit/WithdrawInfo"
 
 export default function EditPage() {
     const [activeTab, setActiveTab] = useState("프로필")
@@ -21,12 +22,14 @@ export default function EditPage() {
                 return <AccountInfo />
             case "배송지":
                 return <AddressInfo />
+            case "탈퇴하기":
+                return <WithdrawInfo />
             default:
                 return null      
         }
     }
     return <div className="h-full mt-8 w-[var(--content-width)] mx-auto">
-        <MenuTab tabs={["프로필", "계좌", "배송지"]} activeTab={activeTab} onClickTab={onMenuChange} className="border-b border-gray-border"/>
+        <MenuTab tabs={["프로필", "계좌", "배송지", "탈퇴하기"]} activeTab={activeTab} onClickTab={onMenuChange} className="border-b border-gray-border"/>
         <div className="w-full ax-w-6xl pt-10 h-full">
             {renderContent()}
         </div>
