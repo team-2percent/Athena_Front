d930ea14ac0f7f4b68b17d4daf3bd20fa6941b19
LeeJinyoung218 <amy021899@gmail.com>
2025-06-08 05:22:52 +0900

    feat: 카테고리 프로젝트 목록 Cypress 테스트 추가

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -3,13 +3,18 @@
 import CategoryMenu from "@/components/list/CategoryMenu";
 import ListPage from "@/components/list/ListPage";
 import { useParams } from "next/navigation";
+import { useState } from "react";
 
 export default function CategoryPage() {
+    const [categoryError, setCategoryError] = useState(false)
+    const handleCategoryError = () => {
+        setCategoryError(true)
+    }
     return (
         <div className="flex flex-col items-center w-[var(--content-width)]">
             {/* 카테고리 메뉴 */}
-            <CategoryMenu categoryId={0} />
-            <ListPage type="category" categoryId={0} />
+            <CategoryMenu categoryId={0} handleCategoryError={handleCategoryError} />
+            {!categoryError && <ListPage type="category" categoryId={0} />}
         </div>
     )
 }
