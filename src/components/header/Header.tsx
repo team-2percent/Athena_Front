c8d0572280dc7050de5b936bbd13e93435c54955
LeeJinyoung218 <amy021899@gmail.com>
2025-06-08 05:42:25 +0900

    feat: 관리자 상호작용 Cypress 테스트 추가 및 프로젝트 승인 목록 JSON 파일 생성

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -280,6 +280,7 @@ const Header = () => {
                           type="button"
                           onClick={() => router.push("/admin/approval")}
                           className="text-sm text-gray-500 hover:text-gray-700 whitespace-nowrap flex items-center gap-2 p-2 justify-start"
+                          data-cy="adminpage-button"
                         >
                           <UserLock className="h-4 w-4" />
                           관리자페이지
