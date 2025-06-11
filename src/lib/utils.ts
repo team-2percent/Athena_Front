601538641cc4565efa1e359259618c9c80631b47
7minseok7 <mansang99@gmail.com>
2025-06-11 14:13:20 +0900

    fix: utils.ts 문법 오류 수정

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,6 +50,7 @@ export function formatDate(date: Date): string {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   return `${year}. ${month}. ${day}.`;
+}
 
 export function getRidOfNonNumber(str: string): string {
   return str.replace(/[^\d]/g, "")
