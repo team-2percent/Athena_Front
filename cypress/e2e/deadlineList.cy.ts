627dc5f5f2b9460e44b6a1f4ac5856dec167c9ed
LeeJinyoung218 <amy021899@gmail.com>
2025-06-23 00:10:51 +0900

    fix: Cypress 마감임박 리스트 테스트 수정

--- cypress/e2e/deadlineList.cy.ts
+++ cypress/e2e/deadlineList.cy.ts
@@ -1,169 +1,263 @@
 import { listType, sortName } from '../../src/lib/listConstant';
 
-describe("마감 임박 프로젝트 목록", () => {
+describe("마감임박 프로젝트 조회", () => {
     beforeEach(() => {
+        // given - 마감임박 프로젝트 페이지 방문
         cy.fixture('list/projectList.json').then((projectList) => {
-            cy
-            .intercept('GET', '/api/project/deadlineList?sortTypeDeadline=*', projectList)
-            .as('getDeadlineProjectList');
-        });
-
-        cy.fixture('list/nextProjectList.json').then((nextProjectList) => {
-            cy
-            .intercept('GET', '/api/project/deadlineList?sortTypeDeadline=*&cursorValue=*&lastProjectId=*', nextProjectList)
-            .as('getNextDeadlineProjectList');
-        });
-
-        cy.visitMainPage();
-    })
-
-    describe("마감 임박 프로젝트 목록 조회", () => {
-        it("마감 임박 프로젝트 목록 조회 성공", () => {
-            // given - 마감 임박 프로젝트 목록 접근
-            cy.get('[data-cy="menu-tab"]').get('[data-cy="menu-tab-마감임박"]').click()
-            cy.url().should('include', '/deadline')
-
-            // when - 마감 임박 프로젝트 목록 조회 성공
-            cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
-
-            // then - 마감 임박 프로젝트 목록 20개 확인
-            cy.get('[data-cy="list-header"]').should('be.visible')
-            cy.get('[data-cy="list-header-count"]').should('be.visible').should('contain', '33')
-            cy.get('[data-cy="project-list"]').should('be.visible')
-            cy.get('[data-cy="project-list-item"]').should('have.length', 20)
-        })
-
-        it("마감 임박 프로젝트 목록 조회 성공, 데이터가 없을 때", () => {
             cy.intercept({
                 method: 'GET',
-                url: '/api/project/deadlineList?sortTypeDeadline=*'
+                url: '/api/project/deadlineList?sortTypeDeadline=*',
             }, {
                 statusCode: 200,
-                body: {
-                    content: [],
-                    nextCursorValue: null,
-                    nextProjectId: null,
-                    total: 0
-                }
+                body: projectList,
             }).as('getDeadlineProjectList')
-
-            // given - 마감 임박 프로젝트 목록 접근
-            cy.get('[data-cy="menu-tab"]').get('[data-cy="menu-tab-마감임박"]').click()
-            cy.url().should('include', '/deadline')
-
-            // when - 마감 임박 프로젝트 목록 조회 성공, 데이터가 없을 때
-            cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
-
-            // then - 빈 메시지 확인
-            cy.get('[data-cy="empty-message-card"]').should('be.visible')
-            cy.get('[data-cy="empty-message"]').should('be.visible').should('contain', '프로젝트가 없습니다.')
         })
-
-        it("서버 오류로 인한 마감 임박 프로젝트 목록 조회 실패", () => {
-            cy.intercept({
-                method: 'GET',
-                url: '/api/project/deadlineList?sortTypeDeadline=*'
-            }, {
-                statusCode: 500
-            }).as('getDeadlineProjectList')
-
-            // given - 마감 임박 프로젝트 목록 접근
-            cy.get('[data-cy="menu-tab"]').get('[data-cy="menu-tab-마감임박"]').click()
-            cy.url().should('include', '/deadline')
-
-            // when - 마감 임박 프로젝트 목록 조회 실패
-            cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 500)
-    
-            // then - 마감 임박 프로젝트 목록 조회 실패
-            cy.get('[data-cy="server-error-card"]').should('be.visible')
-            cy.get('[data-cy="server-error-message"]').should('be.visible').should('contain', '프로젝트를 불러오는 중 오류가 발생했습니다.')
-        })
+        cy.visit('/deadline')
+        cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
     })
 
-    describe("마감 임박 프로젝트 목록 추가 조회", () => {
-        beforeEach(() => {
-            cy.get('[data-cy="menu-tab"]').get('[data-cy="menu-tab-마감임박"]').click()
-            cy.url().should('include', '/deadline')
-        })
+    describe("정렬 select 선택", () => {
+        describe("정렬 표시", () => {
+            it("'/deadline' 방문", () => {
+                // then - 정렬 select visible 확인, 최신순 텍스트 확인
+                cy.get('[data-cy="list-header-sort-button"]').should('contain', '마감순')
 
-        it("마감 임박 프로젝트 목록 추가 조회 성공", () => {
-            // when - 스크롤 이동, 마감 임박 프로젝트 목록 추가 조회 성공
-            cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                // when - 정렬 select 클릭
+                cy.get('[data-cy="list-header-sort-button"]').click()
 
-            cy.scrollTo('bottom')
-            cy.get('[data-cy="loading-spinner"]').should('be.visible')
-
-            // then - 마감 임박 프로젝트 목록 33개 확인
-            cy.wait('@getNextDeadlineProjectList', { timeout: 10000 }).its('response.statusCode').should('eq', 200)
-
-            cy.get('[data-cy="project-list"]').should('be.visible')
-            cy.get('[data-cy="project-list-item"]').should('have.length', 33)
-
-            // loading spinner 사라짐
-            cy.get('[data-cy="loading-spinner"]').should('not.exist')
-        })
-
-        it("서버 오류로 인한 마감 임박 프로젝트 목록 추가 조회 실패", () => {
-            cy.intercept({
-                method: 'GET',
-                url: '/api/project/deadlineList?sortTypeDeadline=*&cursorValue=*&lastProjectId=*'
-            }, {
-                statusCode: 500
-            }).as('getNextDeadlineProjectList')
-
-            // when - 마감 임박 프로젝트 목록 추가 조회 실패
-            cy.wait('@getDeadlineProjectList', { timeout: 10000 }).its('response.statusCode').should('eq', 200)
-
-            cy.scrollTo('bottom')
-            cy.get('[data-cy="loading-spinner"]').should('be.visible')
-
-            cy.wait('@getNextDeadlineProjectList').its('response.statusCode').should('eq', 500)
-
-            // then - 마감 임박 프로젝트 목록 추가 조회 실패
-            cy.get('[data-cy="server-error-card"]').should('be.visible')
-            cy.get('[data-cy="server-error-message"]').should('be.visible').should('contain', '프로젝트를 불러오는 중 오류가 발생했습니다. 다시 시도 해주세요.')
-        })
-    })
-
-    describe("마감 임박 프로젝트 목록 정렬 변경", () => {
-        beforeEach(() => {
-            cy.get('[data-cy="menu-tab"]').get('[data-cy="menu-tab-마감임박"]').click()
-            cy.url().should('include', '/deadline')
-
-            cy.get('[data-cy="list-header-sort-button"]').should('contain', '마감순').click()
-
-            cy.get('[data-cy="list-header-sort-dropdown"]').should('be.visible')
-            listType.deadline.sort.forEach((sortKey: string) => {
-                cy.get('[data-cy="list-header-sort-dropdown"]').contains(sortName[sortKey as keyof typeof sortName]).should('be.visible')
+                // then - 정렬 select 내 옵션 "마감순", "인기순", "달성률순" 확인
+                cy.get('[data-cy="list-header-sort-dropdown"]').should('be.visible')
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('마감순').should('be.visible')
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('인기순').should('be.visible')
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('달성률순').should('be.visible')
             })
         })
 
-        it("정렬 변경 성공", () => {
-            cy.get('[data-cy="list-header-sort-dropdown"]').contains(sortName["POPULAR"]).click()
+        describe("현재 상태인 최신순 설정 시 아무일도 일어나지 않고 select 닫힘", () => {
+            it("옵션 '마감순' 클릭", () => {
+                // given - API 응답 설정
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        statusCode: 200,
+                        body: projectList,
+                    }).as('getDeadlineProjectList')
+                })
 
-            cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                // given - 페이지 방문 및 초기 로딩 완료
+                cy.visit('/deadline')
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
 
-            cy.get('[data-cy="list-header-sort-dropdown"]').should('not.exist')
-            cy.get('[data-cy="list-header-sort-button"]').should('contain', '인기순')
+                // when - 정렬 select 클릭
+                cy.get('[data-cy="list-header-sort-button"]').click()
+
+                // when - 옵션 '마감순' 클릭
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('마감순').click()
+
+                // then - select 닫힘 확인, select의 text '마감순' 확인
+                cy.get('[data-cy="list-header-sort-dropdown"]').should('not.exist')
+                cy.get('[data-cy="list-header-sort-button"]').should('contain', '마감순')
+            })
         })
 
-        it("서버 오류로 인한 정렬 변경 실패", () => {
-            cy.intercept({
-                method: 'GET',
-                url: '/api/project/deadlineList?sortTypeDeadline=*'
-            }, {
-                statusCode: 500
-            }).as('getNextDeadlineProjectList')
+        describe("인기순 변경 시 API 요청, select 닫힘", () => {
+            it("옵션 '인기순' 클릭", () => {
+                // given - API 응답 설정
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        statusCode: 200,
+                        body: projectList,
+                    }).as('getDeadlineProjectList')
+                })
 
-            cy.get('[data-cy="list-header-sort-dropdown"]').contains(sortName["POPULAR"]).click()
+                // given - 페이지 방문 및 초기 로딩 완료
+                cy.visit('/deadline')
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
 
-            cy.wait('@getNextDeadlineProjectList').its('response.statusCode').should('eq', 500)
+                // when - 정렬 select 클릭
+                cy.get('[data-cy="list-header-sort-button"]').click()
 
-            // then - 마감 임박 프로젝트 목록 정렬 변경 실패
-            cy.get('[data-cy="server-error-card"]').should('be.visible')
-            cy.get('[data-cy="server-error-message"]').should('be.visible').should('contain', '프로젝트를 불러오는 중 오류가 발생했습니다. 다시 시도 해주세요.')
-            cy.get('[data-cy="list-header-sort-dropdown"]').should('not.exist')
-            cy.get('[data-cy="list-header-sort-button"]').should('contain', '인기순')
+                // when - 옵션 '인기순' 클릭
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('인기순').click()
+
+                // then - 인기순 API 요청 확인, select 닫힘 확인, select의 text '인기순' 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                cy.get('[data-cy="list-header-sort-dropdown"]').should('not.exist')
+                cy.get('[data-cy="list-header-sort-button"]').should('contain', '인기순')
+            })
+        })
+
+        describe("달성률순 변경 시 API 요청, select 닫힘", () => {
+            it("옵션 '달성률순' 클릭", () => {
+                // given - API 응답 설정
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        statusCode: 200,
+                        body: projectList,
+                    }).as('getDeadlineProjectList')
+                })
+
+                // given - 페이지 방문 및 초기 로딩 완료
+                cy.visit('/deadline')
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+
+                // when - 정렬 select 클릭
+                cy.get('[data-cy="list-header-sort-button"]').click()
+
+                // when - 옵션 '달성률순' 클릭
+                cy.get('[data-cy="list-header-sort-dropdown"]').contains('달성률순').click()
+
+                // then - 달성률순 API 요청 확인, select 닫힘 확인, select의 text '달성률순' 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                cy.get('[data-cy="list-header-sort-dropdown"]').should('not.exist')
+                cy.get('[data-cy="list-header-sort-button"]').should('contain', '달성률순')
+            })
+        })
+    })
+
+    describe("리스트", () => {
+        describe("조회 로딩 중 프로젝트 건수 표시 문구, 프로젝트 리스트에 스켈레톤 위치", () => {
+            it("프로젝트 GET 요청 중", () => {
+                // given - API 응답 설정 (지연 포함)
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        ...projectList,
+                        delay: 1000
+                    }).as('getDeadlineProjectList')
+                })
+
+                // when - 페이지 방문 (API 요청 시작)
+                cy.visit('/deadline')
+
+                // then - 건수 표시 문구 스켈레톤 visible 확인, 리스트 스켈레톤 visible 확인
+                cy.get('[data-cy="list-header-count-skeleton"]').should('be.visible')
+                cy.get('[data-cy="project-list-skeleton"]').should('be.visible')
+            })
+        })
+
+        describe("조회 시 20개 이상일 때 프로젝트 건수를 표시하고, 20개로 프로젝트 리스트를 표시", () => {
+            it("프로젝트 GET 요청 200", () => {
+                // given - API 응답 설정
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        statusCode: 200,
+                        body: projectList,
+                    }).as('getDeadlineProjectList')
+                })
+
+                // when - 페이지 방문
+                cy.visit('/deadline')
+
+                // then - API 요청 확인, 건수 표시 확인, 리스트 20개 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                cy.get('[data-cy="list-header-count"]').should('be.visible').should('contain', '33')
+                cy.get('[data-cy="project-list-item"]').should('have.length', 20)
+            })
+        })
+
+        describe("마지막 추가 조회 시 20개 이내의 다음 프로젝트를 표시하고, loading bar 없음 확인", () => {
+            it("프로젝트 추가 GET 요청 200", () => {
+                // given - API 응답 설정
+                cy.fixture('list/projectList.json').then((projectList) => {
+                    cy.intercept({
+                        method: 'GET',
+                        url: '/api/project/deadlineList?sortTypeDeadline=*',
+                    }, {
+                        statusCode: 200,
+                        body: projectList,
+                    }).as('getDeadlineProjectList')
+                })
+
+                cy.fixture('list/nextProjectList.json').then((nextProjectList) => {
+                    cy.intercept('GET', '/api/project/deadlineList?sortTypeDeadline=*&cursorValue=*&lastProjectId=*', {
+                        statusCode: 200,
+                        body: nextProjectList,
+                    }).as('getNextProjectList')
+                })
+
+                // given - 페이지 방문 및 초기 로딩 완료
+                cy.visit('/deadline')
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+
+                // when - 맨 아래로 스크롤
+                cy.scrollTo('bottom')
+
+                // then - loading bar visible 확인, 추가 API 요청 확인, 프로젝트 리스트 사이즈 확인, loading bar not.exist 확인
+                cy.get('[data-cy="loading-spinner"]').should('be.visible')
+                cy.wait('@getNextProjectList').its('response.statusCode').should('eq', 200)
+                cy.get('[data-cy="project-list-item"]').should('have.length', 33)
+                cy.get('[data-cy="loading-spinner"]').should('not.exist')
+            })
+        })
+
+        describe("데이터가 없을 시 프로젝트 건수를 0으로 표시하고, 비어있음 메시지 표시", () => {
+            it("프로젝트 GET 요청 200, 데이터 없음", () => {
+                // given - API 응답 설정 (빈 데이터)
+                cy.intercept({
+                    method: 'GET',
+                    url: '/api/project/deadlineList?sortTypeDeadline=*',
+                }, {
+                    statusCode: 200,
+                    body: {
+                        content: [],
+                        nextCursorValue: null,
+                        nextProjectId: null,
+                        total: 0
+                    }
+                }).as('getDeadlineProjectList')
+
+                // when - 페이지 방문
+                cy.visit('/deadline')
+
+                // then - API 요청 확인, 건수 표시 '0' 확인, 리스트 0개 확인, 비어있음 메시지 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 200)
+                cy.get('[data-cy="list-header-count"]').should('be.visible').should('contain', '0')
+                cy.get('[data-cy="project-list-item"]').should('have.length', 0)
+                cy.get('[data-cy="empty-message-card"]').should('be.visible')
+                cy.get('[data-cy="empty-message"]').should('be.visible').should('contain', '프로젝트가 없습니다.')
+            })
+        })
+
+        describe("서버 에러 시 프로젝트 건수를 -으로 표시하고, 서버에러 메시지 표시", () => {
+            it("프로젝트 GET 요청 500", () => {
+                // given - API 응답 설정 (서버 에러)
+                cy.intercept({
+                    method: 'GET',
+                    url: '/api/project/deadlineList?sortTypeDeadline=*',
+                }, {
+                    statusCode: 500
+                }).as('getDeadlineProjectList')
+
+                // when - 페이지 방문
+                cy.visit('/deadline')
+
+                // then - API 요청 500 확인, 건수 표시 '-' 확인, 프로젝트 0개 확인, 서버 에러 메시지 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 500)
+                cy.get('[data-cy="list-header-count"]').should('be.visible').should('contain', '-')
+                cy.get('[data-cy="project-list-item"]').should('have.length', 0)
+                cy.get('[data-cy="server-error-card"]').should('be.visible')
+                cy.get('[data-cy="server-error-message"]').should('be.visible').should('contain', '프로젝트를 불러오는 중 오류가 발생했습니다. 다시 시도 해주세요.')
+
+                // when - 다시시도 버튼 클릭
+                cy.get('[data-cy="retry-button"]').click()
+
+                // then - 똑같은 API 요청 확인
+                cy.wait('@getDeadlineProjectList').its('response.statusCode').should('eq', 500)
+            })
         })
     })
 })
