b705822d51f016522e48ae0727efa4d52d504ee3
LeeJinyoung218 <amy021899@gmail.com>
2025-06-25 14:27:28 +0900

    fix: mockApiResponse 및 mockApiErrorResponse의 매개변수 이름을 operationName에서 endpoint로 변경

--- cypress/support/setupNodeEvents/index.ts
+++ cypress/support/setupNodeEvents/index.ts
@@ -20,9 +20,9 @@ export const e2eNodeEvents: Cypress.Config['e2e']['setupNodeEvents'] = (on) => {
   });
   
   on('task', {
-    async mockApiResponse({ operationName, data}) {
+    async mockApiResponse({ endpoint, data}) {
       try {
-        mockApiServer.mockResponse({ operationName, data });
+        mockApiServer.mockResponse({ endpoint, data });
         return null;
       } catch (error) {
         console.error('Failed to mock API response:', error);
@@ -30,9 +30,9 @@ export const e2eNodeEvents: Cypress.Config['e2e']['setupNodeEvents'] = (on) => {
       }
     },
     
-    async mockApiErrorResponse({ operationName, message }) {
+    async mockApiErrorResponse({ endpoint, message }) {
       try {
-        mockApiServer.mockErrorResponse({ operationName, message });
+        mockApiServer.mockErrorResponse({ endpoint, message });
         return null;
       } catch (error) {
         console.error('Failed to mock API error response:', error);
